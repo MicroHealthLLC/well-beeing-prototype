037c47c4beb547110c688b1a65780c38e645fd5d
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-10-19 15:26:38 -0700

    Add event listener to service worker to listen for notification clicks

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,2 +1,14 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  if (event.action === "view-video") {
+    console.log("VIEW VIDEO");
+    clients.openWindow("https://www.microhealthllc.com/");
+  } else if (event.action == "snooze") {
+    console.log("SNOOZE");
+  } else {
+    console.log("BUTTONS NOT CLICKED");
+  }
+});
